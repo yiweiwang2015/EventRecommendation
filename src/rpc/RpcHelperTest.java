06891fce3096935e1e0104e4dda5c5ca5a83e986
yiweiwang2015 <32394799+yiweiwang2015@users.noreply.github.com>
2019-03-05 00:27:58 -0500

    Add files via upload

---
+++ src/rpc/RpcHelperTest.java
@@ -0,0 +1,42 @@
+package rpc;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
+
+public class RpcHelperTest {
+
+	@Test
+	public void testGetJSONArrayCornerCases() throws JSONException {
+		Set<String> category = new HashSet<String>();
+		category.add("category one");
+		
+		List<Item> listItem = new ArrayList<Item>();
+		JSONArray jsonArray = new JSONArray();
+		JSONAssert.assertEquals(jsonArray, RpcHelper.getJSONArray(listItem), true);
+
+		Item one = new ItemBuilder().setItemId("one").setRating(5).setCategories(category).build();
+		Item two = new ItemBuilder().setItemId("two").setRating(5).setCategories(category).build();
+		listItem.add(one);
+		listItem.add(two);
+		
+		jsonArray.put(one.toJSONObject());
+		jsonArray.put(two.toJSONObject());	
+		JSONAssert.assertEquals(jsonArray, RpcHelper.getJSONArray(listItem), true);
+		
+		Item empty = new ItemBuilder().build();
+		listItem.add(empty);
+		jsonArray.put(empty.toJSONObject());
+		JSONAssert.assertEquals(jsonArray, RpcHelper.getJSONArray(listItem), true);
+	}
+
+}
